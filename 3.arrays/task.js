5128bd92f26ba21e9e663e50f27367feeced9a82
Grigoriy Tuzhikov <t4grisha@gmail.com>
2022-03-09 02:41:07 +0300

    complete tasks

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,6 +1,8 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
+  let result = false;
+    if (arr1.length === arr2.length && arr1.every((n,i) => n === arr2[i])) {
+      result = true;
+    }
   // Ваш код
 
   return result; // boolean
@@ -10,6 +12,7 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
-
+const newArr = arr.filter((number) => number > 0 && number % 3 === 0);
+resultArr = newArr.map((number) => number*10);
   return resultArr; // array
 }
