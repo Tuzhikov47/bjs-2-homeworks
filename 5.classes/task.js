9a9813a72594569ceb095bccf1d7b794cc912987
Grigoriy Tuzhikov <t4grisha@gmail.com>
2022-03-16 13:58:11 +0300

    version with Book

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -36,6 +36,14 @@ class Magazine extends PrintEditionItem {
     }
 }
 
+class Book extends PrintEditionItem{
+    constructor(author,name,releaseDate,pagesCount,) {
+        super(name,releaseDate,pagesCount);
+        this.type = 'book';
+        this.author = 'author';
+    }
+}
+
 class NovelBook extends Book {
     constructor (author,name,releaseDate,pagesCount) {
         super(author,name,releaseDate,pagesCount);
